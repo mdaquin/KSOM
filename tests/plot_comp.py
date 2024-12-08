4213bc4c886bb53e9be49f0129d76b95b8284e6e
mdaquin <mathieu@daquin.net>
2024-12-08 16:06:08 +0100

    included quicksom gpu in graph

--- tests/plot_comp.py
+++ tests/plot_comp.py
@@ -4,9 +4,9 @@ import matplotlib.pyplot as plt
 df1 = pd.read_csv("comp_results_ksom_cpu.csv").rename(columns={"time": "ksom CPU"}).set_index("dim")
 df2 = pd.read_csv("comp_results_ksom_gpu.csv").rename(columns={"time": "ksom GPU"}).set_index("dim")
 df3 = pd.read_csv("comp_results_quicksom_cpu.csv").rename(columns={"time": "quicksom CPU"}).set_index("dim")
-
+df4 = pd.read_csv("comp_results_quicksom_gpu.csv").rename(columns={"time": "quicksom GPU"}).set_index("dim")
 df5 = pd.read_csv("comp_results_minisom.csv").rename(columns={"time": "minisom"}).set_index("dim")
 
-df = pd.concat((df1, df2, df3, df5), axis=1)
+df = pd.concat((df1, df2, df3, df4, df5), axis=1)
 df.plot()
 plt.show()
